2cbf8d884b08e9fc38adf79c174037b43e055284
Yogesh Bisht <Yogeshbisht.2307@gmail.com>
2022-09-26 01:28:38 +0530

    enironment variable added for firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCfaDf9KByvCfRkF6qwDQSNn9ro7UEXVZQ",
-  authDomain: "chat-kodeweich.firebaseapp.com",
-  projectId: "chat-kodeweich",
-  storageBucket: "chat-kodeweich.appspot.com",
-  messagingSenderId: "604326248133",
-  appId: "1:604326248133:web:f34bc2735507d3f9f7faac",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
